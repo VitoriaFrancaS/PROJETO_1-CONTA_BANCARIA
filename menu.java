ef2010f7e2d6fa093dbe546dcafb555fcde2eab5
Vitoria F <vitoria.s.franca@gmail.com>
2023-11-24 13:05:38 -0300

    Projeto conta bancária

--- menu.java
+++ menu.java
@@ -3,6 +3,7 @@ package contaBancaria;
 import java.util.InputMismatchException;
 import java.util.Scanner;
 
+import conta.controller.contaController;
 import conta.model.conta;
 import conta.util.Cores;
 import conta.model.contaPoupanca;
@@ -11,31 +12,35 @@ import conta.model.contaCorrente;
 public class menu {
 
 	public static void main(String[] args) {
+		
+		contaController contas = new contaController();
 
 				Scanner input = new Scanner(System.in);
 
-				int opcao;
+				int opcao, numero, agencia, tipo, aniversario;
+				String titular;
+				float saldo, limite;
 				
-				 
+				System.out.println("\n Criar Contas\n");
 				
-		        
-				System.out.println(Cores.TEXT_YELLOW + Cores.ANSI_BLACK_BACKGROUND);
+				contaCorrente cc1 = new contaCorrente(contas.gerarNumero(), 123, 1, "João Zulu", 1000f, 100.0f);
+				contas.cadastrar(cc1);
+				
+				contaCorrente cc2 = new contaCorrente(contas.gerarNumero(), 124, 1, "Maria Zulu", 2000f, 100.0f);
+				contas.cadastrar(cc2);
+				
+				contaPoupanca cp1 = new contaPoupanca(contas.gerarNumero(), 125, 2, "Vin Diesel", 4000f, 12);
+				contas.cadastrar(cp1);
+				
+				contaPoupanca cp2 = new contaPoupanca(contas.gerarNumero(), 126, 2, "Groot Rocket", 8000f, 15);
+				contas.cadastrar(cp2);
+					
+				contas.listarTodas();
 
-				contaCorrente cc1 = new contaCorrente(1, 123, 1, "Claudio Roberto", 0.0f, 1000.0f);
-				cc1.visualizar();
-				cc1.sacar(12000.0f);
-				cc1.visualizar();
-				cc1.depositar(5000.0f);
-				cc1.visualizar();
+
 				
-		  
-				contaPoupanca cp1 = new contaPoupanca(2, 123, 2, "Careca Junior", 100000.0f, 15);
-				cp1.visualizar();
-		        cp1.sacar(1000.0f);
-				cp1.visualizar();
-				cp1.depositar(5000.0f);
-				cp1.visualizar();
-				System.out.println("                                                 "+ Cores.TEXT_RESET);
+				
+				
 
 				while (true) {
 
@@ -80,35 +85,69 @@ public class menu {
 					switch (opcao) {
 					case 1:
 						System.out.println(Cores.TEXT_WHITE_BOLD + "Criar Conta\n\n");
+						
+						System.out.println("Digite o número da agência: ");
+						agencia = input.nextInt();
+						System.out.println("Digite o nome do titular: ");
+						input.skip("\\R?");
+						titular = input.nextLine();
+						
+						do {
+							System.out.println("Digite o tipo da conta (1-CC ou 2-CP: ");
+							tipo = input.nextInt();
+						}while(tipo < 1 && tipo > 2);
+						
+						System.out.println("Digite o saldo da conta (R$): ");
+						saldo = input.nextFloat();
+						
+						switch(tipo) {
+						case 1 -> {
+							System.out.println("Digite o limite de crédito (R$): ");
+							limite = input.nextFloat();
+							contas.cadastrar (new contaCorrente (contas.gerarNumero(), agencia, tipo, titular, saldo, limite));
+							}
+						case 2 -> {
+							System.out.println("Digite o dia do aniversário da conta: ");
+							aniversario = input.nextInt();
+							contas.cadastrar (new contaPoupanca (contas.gerarNumero(), agencia, tipo, titular, saldo, aniversario));
 
+							}
+						}
+						keyPress();
 						break;
 					case 2:
 						System.out.println(Cores.TEXT_WHITE_BOLD + "Listar todas as Contas\n\n");
+						contas.listarTodas();
+						keyPress();
 
 						break;
 					case 3:
 						System.out.println(Cores.TEXT_WHITE_BOLD + "Consultar dados da Conta - por número\n\n");
+						
+						keyPress();
+
 
 						break;
 					case 4:
 						System.out.println(Cores.TEXT_WHITE_BOLD + "Atualizar dados da Conta\n\n");
-
+						
+						keyPress();
 						break;
 					case 5:
 						System.out.println(Cores.TEXT_WHITE_BOLD + "Apagar a Conta\n\n");
-
+						keyPress();
 						break;
 					case 6:
 						System.out.println(Cores.TEXT_WHITE_BOLD + "Saque\n\n");
-
+						keyPress();
 						break;
 					case 7:
 						System.out.println(Cores.TEXT_WHITE_BOLD + "Depósito\n\n");
-
+						keyPress();
 						break;
 					case 8:
 						System.out.println(Cores.TEXT_WHITE_BOLD + "Transferência entre Contas\n\n");
-
+						keyPress();
 						break;
 					default:
 						System.out.println(Cores.TEXT_RED_BOLD + "\nOpção Inválida!\n");
@@ -118,6 +157,11 @@ public class menu {
 
 			}
     
+	private static void keyPress() {
+		// TODO Auto-generated method stub
+		
+	}
+
 	public static void sobre() {
 		System.out.println("\n*********************************************************");
 		System.out.println("Projeto Desenvolvido por: ");
